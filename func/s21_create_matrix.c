5eaa258be5b4ba64fe83c9762bfe699ae1d4f663
Angel Maykit <angelmay@ar-m3.chel.21-school.ru>
2025-04-12 18:44:50 +0500

    func

---
+++ func/s21_create_matrix.c
@@ -0,0 +1,32 @@
+#include "../s21_matrix.h"
+
+int s21_create_matrix(int rows, int columns, matrix_t *result){
+    if(rows < 1 || columns < 1 || result == NULL) INNCORRECT_MATRIX;
+
+    result->matrix = (double**)malloc(sizeof(double*)*rows);
+
+    if(result->matrix == NULL) INNCORRECT_MATRIX;
+
+    for(int i = 0; i < rows; i++){
+        result->matrix[i] = (double*)malloc(sizeof(double)*columns);
+
+        if(result->matrix[i] == NULL){
+            for(int j = 0; j < i; j++){
+                free(result->matrix[j]);
+            }
+            free(result->matrix);
+            return INNCORRECT_MATRIX;
+    }
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
+            result->matrix[i][j] = 0;
+        }
+    }
+
+    result->rows = rows;
+    result->columns = columns;
+
+    return OK;
+    }
+}
---
+++ s21_matrix.h
@@ -0,0 +1,20 @@
+#ifndef S21_MATRIX_H
+#define S21_MATRIX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define OK 0
+#define INNCORRECT_MATRIX 1
+
+#define EPS 1e-7
+
+typedef struct matrix_struct {
+    double** matrix;
+    int rows;
+    int columns;
+} matrix_t;
+
+#endif
+
