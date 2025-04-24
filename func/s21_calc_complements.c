ca88d8e7c2517acf3037a90eb907a32e1451f9d7
Angel Maykit <angelmay@ar-m5.chel.21-school.ru>
2025-04-24 11:52:21 +0500

    calc, deter

--- func/s21_calc_complements.c
+++ func/s21_calc_complements.c
@@ -17,5 +17,24 @@ int s21_calc_complements(matrix_t *A, matrix_t *result){
         return INCORRECT_MATRIX;
     }
 
-    
+    if(A->rows == 1){
+        result->matrix[0][0] = 1.0;
+    }
+
+    for(int i = 0; i < A->rows; i++){
+        for(int j = 0; i < A->columns; j++){
+            matrix_t minor_matrix;
+            if(s21_create_matrix(A, i, j, &minor_matrix) != OK){
+                return INCORRECT_MATRIX;
+            }
+            double det = 0.0;
+            if(s21_determinant(&minor_matrix, &det) != OK){
+                return INCORRECT_MATRIX;
+                s21_remove_matrix(&minor_matrix);
+            }
+            result->matrix[i][j] = pow(-1, i+j)*det;
+            s21_remove_matrix(&minor_matrix);
+        }
+    }
+    return OK;
 }
--- func/s21_determinant.c
+++ func/s21_determinant.c
@@ -1,17 +1,13 @@
 #include "../s21_matrix.h"
 
 int s21_determinant(matrix_t *A, double *result){
-      if (A == NULL || A->matrix == NULL) {
+      if (A == NULL || A->matrix == NULL || result == NULL) {
         return  INCORRECT_MATRIX;
     }
 
-    if (A->rows < 1 || A->columns < 1) {
-        return INCORRECT_MATRIX;
-    }
-    
-    if (s21_create_matrix(A->rows, A->columns) != OK){
-        return INCORRECT_MATRIX;
-    }
+  if(A->rows != A->columns || A->rows < 1){
+    return INCORRECT_MATRIX;
+  }
 
     if(A->rows == 1){
         *result = A->matrix[0][0];
@@ -23,8 +19,25 @@ int s21_determinant(matrix_t *A, double *result){
         return OK;
     }
 
-    for(int i = 0; i < A->rows; i++){
+    *result = 0;
+    int status = OK;
+    
+    for(int i = 0; i<A->rows && status == OK;i++){
+        matrix_t *minor_matrix;
+
+        if(s21_create_matrix(A->rows-1,A->rows-1,&minor_matrix) == OK){
+            for(int j = 1;j<A->rows;j++){
+                for(int k= 0, m = 0;k<A->columns;k++){
+                    if(k == 0) continue;
+                    minor_matrix->matrix[j-1][m] = A->matrix[j][k];
+                }
+            }
+            double det = 0;
+            if((status = s21_determinant(minor_matrix,&det))==OK){
+                *result += pow(-1,i)*A->matrix[0][i]*det;
+            }
+            s21_remove_matrix(&minor_matrix);
+        }
     }
-
-
+    return status;
     }
