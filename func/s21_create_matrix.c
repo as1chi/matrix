213022c1ec7b9d39150aea6045372b3693b5061b
as1chi <aantoha048@gmail.com>
2025-06-08 19:08:30 +0500

    gcov

--- func/s21_create_matrix.c
+++ func/s21_create_matrix.c
@@ -10,13 +10,10 @@ int s21_create_matrix(int rows, int columns, matrix_t *result){
     for(int i = 0; i < rows; i++){
         result->matrix[i] = (double*)malloc(sizeof(double)*columns);
 
-        if(result->matrix[i] == NULL){
-            for(int j = 0; j < i; j++){
-                free(result->matrix[j]);
-            }
-            free(result->matrix);
-            return INCORRECT_MATRIX;
-    }
+       if(result->matrix[i] == NULL) {
+    for(int j = 0; j < i; free(result->matrix[j++]));
+    return (free(result->matrix), INCORRECT_MATRIX);
+}
     
     }
 
